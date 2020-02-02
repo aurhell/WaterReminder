a1b281a5b51b8ebf6ac3ad261970811531478aae
Aurélien Girault <me@agirault.com>
2020-02-02 10:10:06 +0100

    Initial Commit

---
+++ WaterReminder/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WaterReminder
+//
+//  Created by Aurélien Girault on 02/02/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
